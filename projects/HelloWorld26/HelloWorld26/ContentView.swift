28ba07aa15aaf095e1fcec3807331d8fca1c8ea7
radude89 <radu.ionut.dan@gmail.com>
2025-06-10 03:20:04 +0300

    Import HelloWorld project

---
+++ projects/HelloWorld26/HelloWorld26/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloWorld26
+//
+//  Created by Radu Dan on 10.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projects/HelloWorld26/HelloWorld26/HelloWorld26App.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorld26App.swift
+//  HelloWorld26
+//
+//  Created by Radu Dan on 10.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorld26App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
